4644c80cf0fb91534bc070ee1e49fb38e2e5dc69
cqs263 <cqs263@163.com>
2021-01-29 12:22:54 +0800

    项目初始化

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace CheckFileMdWu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCreateMd5 = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnSelMd5File = new System.Windows.Forms.Button();
+            this.lbRzMd5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "对比";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(249, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "文件1路径";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "文件1路径";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(352, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "选择文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(352, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "选择文件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(423, 118);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "对比";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbRzMd5);
+            this.groupBox2.Controls.Add(this.btnCreateMd5);
+            this.groupBox2.Controls.Add(this.txtFilePath);
+            this.groupBox2.Controls.Add(this.btnSelMd5File);
+            this.groupBox2.Location = new System.Drawing.Point(12, 136);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(423, 123);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "计算MD5";
+            // 
+            // btnCreateMd5
+            // 
+            this.btnCreateMd5.Location = new System.Drawing.Point(177, 54);
+            this.btnCreateMd5.Name = "btnCreateMd5";
+            this.btnCreateMd5.Size = new System.Drawing.Size(49, 23);
+            this.btnCreateMd5.TabIndex = 0;
+            this.btnCreateMd5.Text = "计算";
+            this.btnCreateMd5.UseVisualStyleBackColor = true;
+            this.btnCreateMd5.Click += new System.EventHandler(this.btnCreateMd5_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(19, 19);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(319, 21);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnSelMd5File
+            // 
+            this.btnSelMd5File.Location = new System.Drawing.Point(352, 19);
+            this.btnSelMd5File.Name = "btnSelMd5File";
+            this.btnSelMd5File.Size = new System.Drawing.Size(65, 23);
+            this.btnSelMd5File.TabIndex = 5;
+            this.btnSelMd5File.Text = "选择文件";
+            this.btnSelMd5File.UseVisualStyleBackColor = true;
+            this.btnSelMd5File.Click += new System.EventHandler(this.btnSelMd5File_Click);
+            // 
+            // lbRzMd5
+            // 
+            this.lbRzMd5.AutoSize = true;
+            this.lbRzMd5.Location = new System.Drawing.Point(89, 89);
+            this.lbRzMd5.Name = "lbRzMd5";
+            this.lbRzMd5.Size = new System.Drawing.Size(53, 12);
+            this.lbRzMd5.TabIndex = 6;
+            this.lbRzMd5.Text = "计算结果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 271);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCreateMd5;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnSelMd5File;
+        private System.Windows.Forms.Label lbRzMd5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckFileMdWu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+        private   string GetMD5HashFromFile(string fileName)
+        {
+            try
+            {
+                FileStream file = new FileStream(fileName, FileMode.Open);
+                System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] retVal = md5.ComputeHash(file);
+                file.Close();
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("获取文件的MD5值出错:"+ ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sMd51 = GetMD5HashFromFile(textBox1.Text.Trim());
+
+            string sMd52 = GetMD5HashFromFile(textBox2.Text.Trim());
+
+            if (sMd51.Equals(sMd52))
+            {
+                MessageBox.Show("这两个文件Md5一样！");
+            }
+            else
+            {
+                MessageBox.Show("这两个文件Md5不一样！");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //初始化一个OpenFileDialog类
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            //判断用户是否正确的选择了文件
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fileDialog.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //初始化一个OpenFileDialog类
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            //判断用户是否正确的选择了文件
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = fileDialog.FileName;
+            }
+        }
+
+        private void btnSelMd5File_Click(object sender, EventArgs e)
+        {
+            //初始化一个OpenFileDialog类
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            //判断用户是否正确的选择了文件
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = fileDialog.FileName;
+            }
+        }
+
+        private void btnCreateMd5_Click(object sender, EventArgs e)
+        {
+            string baseFilePath = @"d:\a.jpg";
+            Bitmap bmpBase = new Bitmap(baseFilePath);
+
+            // 画像を切り抜く
+            Rectangle rect = new Rectangle(261, 81, 170, 170);
+            Bitmap bmpNew = bmpBase.Clone(rect, bmpBase.PixelFormat);
+
+            // 画像をGIF形式で保存
+            string newFilePath = @"d:\ a2.jpg";
+            bmpNew.Save(newFilePath, ImageFormat.Jpeg);
+
+            // 画像リソースを解放
+            bmpBase.Dispose();
+            bmpNew.Dispose();
+
+            //string sMd5 = GetMD5HashFromFile(txtFilePath.Text.Trim());
+            //lbRzMd5.Text = sMd5;
+        }
+    }
+}
